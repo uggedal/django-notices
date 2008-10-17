f94315a988616438acac3f15fd704348070047c9
Eivind Uggedal <eu@redflavor.com>
2008-10-17 14:34:18 +0200

    Initial messages version. Without tests and docs.

---
+++ notices/__init__.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponseRedirect
+from django.conf import settings
+import urllib, base64, re
+
+def allowed_notice_types():
+    if hasattr(settings, 'URLMESSAGE_TYPES'):
+        return settings.URLMESSAGE_TYPES
+    else:
+        return ('success', 'notice', 'error')
+
+def pack(str):
+    return urllib.quote_plus(base64.b64encode(str))
+
+def unpack(str):
+    return urllib.unquote_plus(base64.b64decode(str))
+
+class HttpResponseRedirectWithNotice(HttpResponseRedirect):
+
+    def __init__(self, redirect_to, **kwargs):
+        self.url = redirect_to
+        self.process_notices(kwargs)
+
+        HttpResponseRedirect.__init__(self, self.url)
+
+    def process_notices(self, notices):
+        for type, notice in notices.iteritems():
+            if type in allowed_notice_types():
+                self.append_notice(type, notice)
+
+    def append_notice(self, type, notice):
+        if re.match("\?", self.url):
+            self.url += "&_%s=%s" % (type, pack(notice))
+        else:
+            self.url += "?_%s=%s" % (type, pack(notice))
