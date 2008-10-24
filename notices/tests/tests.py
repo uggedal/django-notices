c85000c3cc84478863e255da047120ea1dfd0ba4
Eivind Uggedal <eu@redflavor.com>
2008-10-24 15:16:44 +0200

    Should only allow the default or defined notice types.

--- notices/tests/tests.py
+++ notices/tests/tests.py
@@ -1,15 +1,31 @@
 from django.test import TestCase
 from django.http import QueryDict
+from django.conf import settings
 from urlparse import urlsplit
 
-class NoticesTestCase(TestCase):
+class NoticesTestHelper(TestCase):
+    notice_identifier = '<ul class="notices">'
 
-    def _get_with_redirect(self, location):
+    def assert_contains_notices(self, request):
+        self.assertContains(request, self.notice_identifier)
+
+    def assert_not_contains_notices(self, request):
+        self.assertNotContains(request, self.notice_identifier)
+
+    def get_with_redirect(self, location):
         request = self.client.get(location)
         scheme, netloc, path, query, fragment = urlsplit(request['Location'])
         redirected_request = self.client.get(path, QueryDict(query))
         return (request, redirected_request)
 
+class NoticesTestCase(NoticesTestHelper):
+
     def test_should_display_notices(self):
-        r1, r2 = self._get_with_redirect('/redirect_with_notice/')
-        self.assertContains(r2, '<ul class="notices">')
+        r1, r2 = self.get_with_redirect('/redirect_with_notice/')
+        self.assert_contains_notices(r2)
+
+    def test_should_not_display_unsupported_notice_types(self):
+        settings.NOTICE_TYPES = ('soletype')
+        r1, r2 = self.get_with_redirect('/redirect_with_notice/')
+        self.assert_not_contains_notices(r2)
+        settings.NOTICE_TYPES = None
