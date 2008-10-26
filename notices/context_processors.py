b3808820923c335f94d9eddfd891ad3ee8975104
Eivind Uggedal <eu@redflavor.com>
2008-10-26 12:25:31 +0100

    Context processor for disabling the core django messaging system.

--- notices/context_processors.py
+++ notices/context_processors.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.core.context_processors import PermWrapper
 from notices import unpack, allowed_notice_types
 import re
 
@@ -14,3 +17,27 @@ def notices(request):
             return {}
     except (ValueError, TypeError):
         return {}
+
+def auth_without_messages(request):
+    """
+    A modified version of the bundled authentication context processor
+    which does not retrieve per-user messages from the database.
+
+    Be aware that the admin currently does not work when the default
+    auth context processor is disabled when running in DEBUG mode.
+    You will also loose feedback given with messages in the admin.
+    """
+
+    if settings.DEBUG and 'django.core.context_processors.auth' in settings.TEMPLATE_CONTEXT_PROCESSORS:
+            raise ImproperlyConfigured("'django.core.context_processors.auth' can not be used together with 'notices.context_processors.auth_without_messages'")
+
+    if hasattr(request, 'user'):
+        user = request.user
+    else:
+        from django.contrib.auth.models import AnonymousUser
+        user = AnonymousUser()
+    return {
+        'user': user,
+        'messages': [],
+        'perms': PermWrapper(user),
+    }
